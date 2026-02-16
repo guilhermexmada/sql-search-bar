13dbfb8d6e7f32ed19a83c25d4a3d48a4576ef83
Shimada <guilhermeshimadapereira@gmail.com>
2026-02-16 18:23:14 -0300

    preparando ambiente node & express

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express"
+
+// instanciando express
+const app = express()
+
+// configurando view engine
+app.set("view engine", "ejs")
+
+// rota raiz
+app.get("/", (req, res) => {
+    res.send('hellow world')
+})
+
+// inicia servidor HTTP
+const port = 8080
+app.listen(port, (error) => {
+    if (error) {
+        console.log(`Oops! Não foi possível iniciar o servidor: ${error}`)
+    } else {
+        console.log(`Servidor iniciado com sucesso em http://localhost:${port}`)
+    }
+})
